72d13d1658dba33b64d11a2c8b874c25f3867e5d
Monlisa20 <67744651+Monlisa20@users.noreply.github.com>
2020-07-03 22:09:46 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+window.onscroll = function scrollFunction() {
+  let mybutton = document.getElementById('to-top');
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.right = '30px';
+  } else {
+    mybutton.style.right = '-70px';
+  }
+}
+
+function findPos(item) {
+  var curtop = 0;
+  if (item.offsetParent) {
+    do {
+      curtop += item.offsetTop;
+    }
+    while (item = item.offsetParent);
+    return [curtop];
+  }
+}
+
+const scrollLink = (id)=> {
+  var a = document.getElementById(id);
+  window.scroll({
+    top: findPos(a) - 120,
+    behavior: 'smooth'
+  });
+  var elm = document.getElementById('menu-toggle');
+  if (elm.checked = true) {elm.click();}
+}
